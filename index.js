5b1da434c17a4ccad94bd3807b488d8ec3839279
rajprajapat1992 <raj.kumar@prilient.com>
2024-09-19 18:11:58 +0530

    product add and delete api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require('./db/config');
 const User = require("./db/Users");
+const Product = require("./db/Product")
 const cors = require('cors');
 const app = express();
 app.use(express.json());
@@ -10,6 +11,13 @@ const Jwt = require('jsonwebtoken');
 const jwtKey = 'e-Comm';
 
 app.post('/register', async (req, resp) => {
+    const { email } = req.body;
+    let exists = await User.findOne({email})
+    if(exists){ 
+        return resp.status(404).json({
+        status : 404,
+        message : "Data already exists."
+    })}
     let user = new User(req.body)
     let result = await user.save();
     Jwt.sign({ result }, jwtKey, (err, token) => {
@@ -35,6 +43,25 @@ app.post('/login', async (req, resp) => {
     } else {
         resp.send({ resul: 'No Data Found' })
     }
+})
+
+app.post("/add-product", async (req, resp) =>{
+    let product = new Product(req.body);
+    let result = await product.save();
+    resp.send(result)
+})
+
+app.get("/products", async (req, resp) =>{
+    let products = await Product.find();
+    if(products.length > 0){
+        resp.send(products)
+    }else{
+        resp.send({message: 'no product found'})
+    }
+})
+app.delete("/delete/:id", async (req, resp) =>{
+    const result = await Product.deleteOne({_id: req.params.id});
+    resp.send(result)
 })
 
 app.listen(4500)
